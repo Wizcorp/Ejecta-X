f866644b3bcfa7aaffa662b2f1cc7f0052dc0127
aogilvie <aogilvie@wizcorp.jp>
2014-03-31 15:51:27 +0900

    re-styled Uri.h

--- sources/ejecta/uriparser/Uri.h
+++ sources/ejecta/uriparser/Uri.h
@@ -1,13 +1,11 @@
 #include <string>
-#include <algorithm>    // find
+#include <algorithm> // find
 
-struct Uri
-{
+struct Uri {
     public:
-    std::string QueryString, Path, Protocol, Host, Port;
+        std::string QueryString, Path, Protocol, Host, Port;
 
-    static Uri Parse(const std::string &uri)
-    {
+    static Uri Parse(const std::string &uri) {
         Uri result;
 
         typedef std::string::const_iterator iterator_t;
@@ -17,56 +15,55 @@ struct Uri
 
         iterator_t uriEnd = uri.end();
 
-        // get query start
+        // Get query start
         iterator_t queryStart = std::find(uri.begin(), uriEnd, '?');
 
-        // protocol
+        // Protocol
         iterator_t protocolStart = uri.begin();
-        iterator_t protocolEnd = std::find(protocolStart, uriEnd, ':');            //"://");
+        // "://"
+        iterator_t protocolEnd = std::find(protocolStart, uriEnd, ':');
 
-        if (protocolEnd != uriEnd)
-        {
+        if (protocolEnd != uriEnd) {
             std::string prot = &*(protocolEnd);
-            if ((prot.length() > 3) && (prot.substr(0, 3) == "://"))
-            {
+            if ((prot.length() > 3) && (prot.substr(0, 3) == "://")) {
                 result.Protocol = std::string(protocolStart, protocolEnd);
-                protocolEnd += 3;   //      ://
+                protocolEnd += 3;   // ://
+            } else {
+                // No protocol
+                protocolEnd = uri.begin();
             }
-            else
-                protocolEnd = uri.begin();  // no protocol
+        } else {
+            // No protocol
+            protocolEnd = uri.begin();
         }
-        else
-            protocolEnd = uri.begin();  // no protocol
 
-        // host
+        // Host
         iterator_t hostStart = protocolEnd;
-        iterator_t pathStart = std::find(hostStart, uriEnd, '/');  // get pathStart
-
-        iterator_t hostEnd = std::find(protocolEnd, 
-            (pathStart != uriEnd) ? pathStart : queryStart,
-            ':');  // check for port
+        // Get pathStart
+        iterator_t pathStart = std::find(hostStart, uriEnd, '/');
+        // Check for port
+        iterator_t hostEnd = std::find(protocolEnd,
+            (pathStart != uriEnd) ? pathStart : queryStart, ':');
 
         if (!result.Protocol.empty()) {
-                result.Host = std::string(hostStart, hostEnd);
+            result.Host = std::string(hostStart, hostEnd);
         }
-
-        // port
-        if ((hostEnd != uriEnd) && ((&*(hostEnd))[0] == ':'))  // we have a port
-        {
+        // Port
+        if ((hostEnd != uriEnd) && ((&*(hostEnd))[0] == ':')) {
+            // We have a port
             hostEnd++;
             iterator_t portEnd = (pathStart != uriEnd) ? pathStart : queryStart;
             result.Port = std::string(hostEnd, portEnd);
         }
-
-        // path
-        if (pathStart != uriEnd)
+        // Path
+        if (pathStart != uriEnd) {
             result.Path = std::string(pathStart, queryStart);
-
-        // query
-        if (queryStart != uriEnd)
+        }
+        // Query
+        if (queryStart != uriEnd) {
             result.QueryString = std::string(queryStart, uri.end());
-
+        }
         return result;
 
-    }   // Parse            
-};  // uri
+    }
+};
